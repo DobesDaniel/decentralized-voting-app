5eff1d23f16824419afb2411cb7abe02e03fe386
DobesDaniel <dobes.daniel@outlook.cz>
2024-05-09 18:29:42 +0200

    Updates

---
+++ precache-manifest.a5d9cdc828130dc2ef97704631d53ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f1573275008cbe5d3a865a47d684fd",
+    "url": "/decentralized-voting-app/index.html"
+  },
+  {
+    "revision": "8ecb3fab7ef824ec2057",
+    "url": "/decentralized-voting-app/static/css/main.47cf4a9d.chunk.css"
+  },
+  {
+    "revision": "65b098f13aff3994d303",
+    "url": "/decentralized-voting-app/static/js/2.21972a62.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/decentralized-voting-app/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "8ecb3fab7ef824ec2057",
+    "url": "/decentralized-voting-app/static/js/main.04ca9293.chunk.js"
+  },
+  {
+    "revision": "0e3569710cb265697565",
+    "url": "/decentralized-voting-app/static/js/runtime~main.5a3e3c2d.js"
+  }
+]);
